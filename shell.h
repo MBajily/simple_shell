255104fd42a3584359493dd1efea9854cdb4aaf3
Mohammed Elgaily <MBajily@gmail.com>
2023-11-15 20:26:49 +0300

    Fix butty style errors

--- shell.h
+++ shell.h
@@ -9,44 +9,44 @@
 #include <sys/wait.h>
 
 
-// Function to free the memory allocated for an array of strings
+/* free_array - Function to free the memory allocated for an array of strings */
 void free_array(char **arr);
-// Function to free the memory allocated for various arrays and strings
+/* free_c - Function to free the memory allocated for various arrays and strings */
 void free_c(char ***args, char **input, char *dir, int alert);
 
-// Function to duplicate a given string
+/* *_str_duplicate - Function to duplicate a given string */
 char *_str_duplicate(const char *str);
-// Function to calculate the length of a string
+/* _str_length - Function to calculate the length of a string */
 size_t _str_length(const char *str);
-// Function to copy the contents of one string to another
+/* _str_copy - Function to copy the contents of one string to another */
 void _str_copy(char *dest, const char *src);
-// Function to concatenate the contents of two strings
+/* _strcat - Function to concatenate the contents of two strings */
 void _strcat(char *dest, const char *src);
-// Function to compare two strings
+/* _strcmp - Function to compare two strings */
 int _strcmp(const char *str1, const char *str2);
 
-// Function to read a line of input from the user
+/* _get_line - Function to read a line of input from the user */
 int _get_line(char **input, size_t *length);
-// Function to parse the command-line arguments from a string
+/* **parse_args - Function to parse the command-line arguments from a string */
 char **parse_args(char *input);
-// Function to check if a given input string is empty
+/* empty_checker - Function to check if a given input string is empty */
 int empty_checker(char *input);
-// Function to check if the input command is for changing the directory
+/* cd_checker - Function to check if the input command is for changing the directory */
 int cd_checker(char ***args, char **input, int index);
-// Function to check if the input command requires a path resolution
+/* path_checker - Function to check if the input command requires a path resolution */
 int path_checker(char **args);
 
-// Function to check if the input command is for exiting the shell
+/* exit_checker - Function to check if the input command is for exiting the shell */
 void exit_checker(char ***args, char **input, int index);
-// Function to display an exit message and clean up resources before exiting the shell
+/* exit_message - Function to display an exit message and clean up resources before exiting the shell */
 void exit_message(char ***args, char **input, int *index);
 
-// Function to check if a given string represents a digit
+/* is_digit - Function to check if a given string represents a digit */
 int is_digit(char *str);
-// Function to check if a given string represents a letter
+/* is_letter - Function to check if a given string represents a letter */
 int is_letter(char *str);
 
-// Function to execute a command with the given arguments and input
+/* execution - Function to execute a command with the given arguments and input */
 int execution(char **args, char *input);
 
 #endif
